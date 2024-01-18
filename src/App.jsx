0c93a8557b2f3f3629ddc606824bfd8170006a7e
Your Name <you@example.com>
2024-01-18 22:17:47 +0100

    add filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState  , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
-import { useDispatch , useSelector } from 'react-redux'
-import { addUser } from './store/UsersSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser, filterUsers } from './store/UsersSlice'
 
 function App() {
 
-  const [code , setCode] = useState("")
-  const [name , setName] = useState("")
-  const [location , setLocation] = useState("")
-  const [avatar  , setAvatar] = useState()
+  const [code, setCode] = useState("")
+  const [name, setName] = useState("")
+  const [location, setLocation] = useState("")
+  const [avatar, setAvatar] = useState()
 
 
   const dispatch = useDispatch()
-  const users = useSelector((store)=>{
-    return store.userState.users 
+  const users = useSelector((store) => {
+    return store.userState.users
   })
 
+  const filtredUsers = useSelector((state) => state.userState.usersFiltred)
 
 
-  const handelChange  = (e)=>{
+
+  const handelChange = (e) => {
     const target = e.target.id
-    if(target == "name"){
+    if (target == "name") {
       setName(e.target.value)
-    }else if(target == "code"){
+    } else if (target == "code") {
       setCode(e.target.value)
-    }else if(target == "location"){
+    } else if (target == "location") {
       setLocation(e.target.value)
-    }else if(target == "img"){
+    } else if (target == "img") {
       const reader = new FileReader()
-      reader.onload = ()=>{
+      reader.onload = () => {
         setAvatar(reader.result)
       }
       reader.readAsDataURL(e.target.files[0])
@@ -37,56 +39,74 @@ function App() {
   }
 
 
-  const handelClick = ()=>{
+  const handelClick = () => {
     dispatch(addUser(
       {
-        name : name , 
-        code : code , 
-        location  :location , 
-        avatar : avatar
+        name: name,
+        code: code,
+        location: location,
+        avatar: avatar
       }
     ))
+  }
+  const handleFilter = () => {
+    const value = prompt("Filter Location : ...")
+    dispatch(filterUsers(value))
   }
 
 
   return (
     <>
       <form>
-      <input type="number" id="code" placeholder='Code : ' onChange={handelChange} />
-      <br></br>
-      <input type="text" id="name" placeholder='name : ' onChange={handelChange} />
-      <br></br>
-      <input type="text" id="location" placeholder='location : ' onChange={handelChange}/>
-      <br></br>
-      <input type="file" id="img" accept='image/*' placeholder='avatar : ' onChange={handelChange} />
-      <br></br>
-      <button type='button' onClick={handelClick} >Submit</button>
-      
+        <input type="number" id="code" placeholder='Code : ' onChange={handelChange} />
+        <br></br>
+        <input type="text" id="name" placeholder='name : ' onChange={handelChange} />
+        <br></br>
+        <input type="text" id="location" placeholder='location : ' onChange={handelChange} />
+        <br></br>
+        <input type="file" id="img" accept='image/*' placeholder='avatar : ' onChange={handelChange} />
+        <br></br>
+        <button type='button' onClick={handelClick} >Submit</button>
+        <button type='button' onClick={handleFilter} >Filter</button>
       </form>
-<hr></hr>
-<table className="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">Avatar</th>
-      <th scope="col">Code</th>
-      <th scope="col">Name</th>
-      <th scope="col">Location</th>
-    </tr>
-  </thead>
-  <tbody>
-  {users.map((ele)=>{
+      <hr></hr>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Avatar</th>
+            <th scope="col">Code</th>
+            <th scope="col">Name</th>
+            <th scope="col">Location</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
 
-    return(   <tr>
-      <th scope="row"> <img className="img" src={ele.avatar} />  </th>
-      <td>{ele.code}</td>
-      <td>{ele.name}</td>
-      <td>{ele.location}</td>
-    </tr>)
-  })}
-  
+          {
+            filtredUsers.length === 0 ? users.map((ele) => {
+              return (<tr>
+                <th scope="row"> <img className="img" src={ele.avatar} />  </th>
+                <td>{ele.code}</td>
+                <td>{ele.name}</td>
+                <td>{ele.location}</td>
+              </tr>)
+            })
 
-  </tbody>
-</table>
+              :
+
+              filtredUsers.map((ele) => {
+                return (<tr>
+                  <th scope="row"> <img className="img" src={ele.avatar} />  </th>
+                  <td>{ele.code}</td>
+                  <td>{ele.name}</td>
+                  <td>{ele.location}</td>
+                </tr>)
+              })
+          }
+
+
+        </tbody>
+      </table>
 
 
 
--- src/store/UsersSlice.js
+++ src/store/UsersSlice.js
@@ -5,16 +5,25 @@ const UserSlice = createSlice({
     name: "user",
     initialState : {
         users : [] , 
+        usersFiltred : []
     } , 
     reducers : {
         addUser : (state, action)=>{
             state.users.push(action.payload)
+        } , 
+        filterUsers : (state , action)=>{
+            if(action.payload){
+                state.usersFiltred = state.users.filter((ele)=> ele.location ===  action.payload)
+            }else{
+                state.usersFiltred = [] // 0
+            }
         }
+
     }
 })
 
 
-export const {addUser} = UserSlice.actions
+export const {addUser , filterUsers} = UserSlice.actions
 
 
 export default UserSlice.reducer
